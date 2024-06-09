f0b2feb4d28197b76256839f898fdae0d8896dc0
Vitor Veras <vitorverasm@gmail.com>
2024-06-09 07:12:01 -0300

    feat: create server and register login route with authentication

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,19 +5,38 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/vitorverasm/my-community/pkg/supabase"
+	"github.com/vitorverasm/my-community/types"
 )
 
 func main() {
-	Initialize()
+	InitializeAPI()
 }
 
-func Initialize() {
+func InitializeAPI() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
+
+	r.POST("/login", func(c *gin.Context) {
+		var loginRequestBody types.LoginRequestBody
+		c.BindJSON(&loginRequestBody)
+
+		supabaseClient := supabase.InitializeClient()
+
+		token, error := supabaseClient.Auth.SignInWithEmailPassword(loginRequestBody.Email, loginRequestBody.Password)
+
+		if error != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": error.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"email": loginRequestBody.Email, "token": token.AccessToken})
+	})
+
 	r.Run(":3000")
 	log.Println("Running server on port 3000...")
 }
